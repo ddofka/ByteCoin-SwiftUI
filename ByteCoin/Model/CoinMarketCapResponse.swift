2b85f4f75b4930b05efffaf63f6edb1c76014c0d
Dovydas <ddorofejevas@yahoo.com>
2025-11-23 23:39:15 +0200

    Remove Secret.plist from repo and ignore it

---
+++ ByteCoin/Model/CoinMarketCapResponse.swift
@@ -0,0 +1,9 @@
+//
+//  CoinMarketCapResponse.swift
+//  ByteCoin
+//
+//  Created by Dovydas Dorofejevas on 23/11/2025.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+import Foundation
